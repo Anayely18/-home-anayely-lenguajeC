27e736725a4dd1f95bb52077a113e90b8f309b48
Anayely18 <alissamorros18@gmail.com>
2022-08-19 04:25:50 -0400

    Procesos en C

---
+++ proceso_hijo.c
@@ -0,0 +1,16 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char*argv[] ) 
+{ 
+    pid_t pid;
+    if((pid=fork())==0 {
+        printf("soy el hijo (%d, hijo de %d)\n", getpid(),
+        getppid());
+    }else {
+        printf("soy el padre (%d, padre de %d)\n", getpid(),getppid());
+    }
+    return 0;
+}
+
